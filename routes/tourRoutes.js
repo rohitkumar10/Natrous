e37bcff2a405206f2283e8ac591e802b6b0380c0
ROHIT KUMAR <rkjust15220041@gmail.com>
2020-06-27 23:38:48 +0530

    Adding new Tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -14,7 +14,17 @@ router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router.route('/').get(authController.protect, tourController.getAllTours).post(tourController.createTour);
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect, authController.restrictTo('admin','lead-guide'), tourController.deleteTour);
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.uploadTourImages,
+        tourController.resizeTourImages,
+        tourController.updateTour
+    )
+    .delete(authController.protect, authController.restrictTo('admin','lead-guide'), tourController.deleteTour);
 // router.route('/:tourId/reviews').post(authController.protect, authController.restrictTo('user'), reviewController.createReview)
 
 module.exports = router;

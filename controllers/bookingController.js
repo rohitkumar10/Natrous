7500fd8b4c5ba47f84e329fdb3ded8f9a078fead
ROHIT KUMAR <rkjust15220041@gmail.com>
2020-06-28 11:30:42 +0530

    Adding Booking api

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
-//const Booking = require('../models/bookingModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
@@ -36,3 +36,19 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       session
     });
 })
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+    // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+    const { tour, user, price } = req.query;
+  
+    if (!tour && !user && !price) return next();
+    await Booking.create({ tour, user, price });
+  
+    res.redirect(req.originalUrl.split('?')[0]);
+})
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
